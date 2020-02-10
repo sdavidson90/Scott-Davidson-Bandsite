4ee1f739b6ca571a9612a1cdd788acc2e1b194d2
sdavidson90 <badpancakes90@gmail.com>
2020-02-09 20:32:41 -0800

    final push

--- sprint-3/scripts/comments_bio.js
+++ sprint-3/scripts/comments_bio.js
@@ -1,5 +1,5 @@
 const form = document.querySelector(".comments__input-info-form");
-const apiKey = "9c3db58b-4bbe-45f7-b0cf-882039feca965111";
+const apiKey = "9c3db58b-4bbe-45f7-b0cf-882039feca9651111";
 
 //CREATING A FUNCTION TO CREATE NEW COMMENT DOM ELEMENTS
 function displayComments(arr) {
@@ -31,15 +31,17 @@ function displayComments(arr) {
     mainTime.innerText = dateString;
     mainTime.classList.add("comments__new-time");
     subDiv.appendChild(mainTime);
+    
     //REMOVE COMMENT BUTTON AND ADD ID TO ELEMENT OF THE ID OF THE COMMENT
-    const removeButton = document.createElement("button")
-    removeButton.addEventListener("click", event => {
-        let commentID = event.target.id;
-        removePost(commentID)
-    });
-    removeButton.classList.add("comments__new-remove");
-    removeButton.id = arr[key]["id"];
-    mainDiv.appendChild(removeButton);
+    // const removeButton = document.createElement("button")
+    // removeButton.addEventListener("click", event => {
+    //     let commentID = event.target.id;
+    //     removePost(commentID)
+    // });
+    // removeButton.classList.add("comments__new-remove");
+    // removeButton.id = arr[key]["id"];
+    // mainDiv.appendChild(removeButton);
+
     //ADDS THE COMMENT TO THE PARENT DIV
     const mainP = document.createElement("p");
     mainP.innerText = arr[key]["comment"];
@@ -56,14 +58,14 @@ form.addEventListener("submit", newPost => {
     newPost.preventDefault();
     
     //POSTING THE FORM RESULTS TO HEROKU SERVER
-    let newAdd = axios.post("https://project-1-api.herokuapp.com/comments?api_key=9c3db58b-4bbe-45f7-b0cf-882039feca965111", {
+    let newAdd = axios.post("https://project-1-api.herokuapp.com/comments?api_key=9c3db58b-4bbe-45f7-b0cf-882039feca9651111", {
     "name": newPost.target.name.value, 
     "comment": newPost.target.comment.value,
     }
     );
     newAdd.then(() => {
         //AFTER POSTING THE FORM INFO, GRABBING THE UPDATED HEROKU COMMENT DATA TO POST THE NEWEST COMMENT ON THE SITE
-        let arrComment = axios.get("https://project-1-api.herokuapp.com/comments?api_key=9c3db58b-4bbe-45f7-b0cf-882039feca965111")
+        let arrComment = axios.get("https://project-1-api.herokuapp.com/comments?api_key=9c3db58b-4bbe-45f7-b0cf-882039feca9651111")
         .then(response => {
         //USING THE DOM DISPLAY FUNCTION TO CREATE THE ELEMENTS TO DISPLAY COMMENTS AND SORTING BY THE NEWEST TIME STAMP AT THE TOP
         displayComments(
@@ -79,7 +81,7 @@ form.addEventListener("submit", newPost => {
 });  
 
 //GETTING THE DATA FROM HEROKU TO POST ON PAGE LOAD-UP
-const arrComment = axios.get("https://project-1-api.herokuapp.com/comments?api_key=9c3db58b-4bbe-45f7-b0cf-882039feca965111")
+const arrComment = axios.get("https://project-1-api.herokuapp.com/comments?api_key=9c3db58b-4bbe-45f7-b0cf-882039feca9651111")
 arrComment.then(response => {
     //CALLING DOM FUNCTION TO POST THE DATA AND SORT IT BY NEWEST TIMESTAMP AT TOP
     displayComments(
@@ -92,17 +94,17 @@ arrComment.then(response => {
 
 // DELETE COMMENT BUTTON
 
-function removePost(id){
-    axios.delete(`https://project-1-api.herokuapp.com/comments/${id}?api_key=9c3db58b-4bbe-45f7-b0cf-882039feca965111`)
-    .then(response => {
-        axios.get("https://project-1-api.herokuapp.com/comments?api_key=9c3db58b-4bbe-45f7-b0cf-882039feca965111")
-        arrComment.then(response => {
-            //CALLING DOM FUNCTION TO POST THE DATA AND SORT IT BY NEWEST TIMESTAMP AT TOP
-            displayComments(
-                response.data.sort(function(a, b) {
-                    return b.timestamp - a.timestamp;
-                })
-            )
-        });
-    });
-}
+// function removePost(id){
+//     axios.delete(`https://project-1-api.herokuapp.com/comments/${id}?api_key=9c3db58b-4bbe-45f7-b0cf-882039feca9651111`)
+//     .then(response => {
+//         axios.get("https://project-1-api.herokuapp.com/comments?api_key=9c3db58b-4bbe-45f7-b0cf-882039feca9651111")
+//         arrComment.then(response => {
+//             //CALLING DOM FUNCTION TO POST THE DATA AND SORT IT BY NEWEST TIMESTAMP AT TOP
+//             displayComments(
+//                 response.data.sort(function(a, b) {
+//                     return b.timestamp - a.timestamp;
+//                 })
+//             )
+//         });
+//     });
+// }
